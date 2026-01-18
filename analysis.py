7575443ddca3ab86908e3d28e0517a818733ff81
Kateryna_Rotan <caterinarotan@gmail.com>
2026-01-18 20:32:18 +0200

    added analysis file

---
+++ analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = {
+    "city": ["Kyiv", "Lviv", "Odesa"],
+    "sales": [1200, 900, 500]
+}
+
+df = pd.DataFrame(data)
+
+print("Продажі по містах:")
+print(df)
+
+average_sales = df["sales"].mean()
+print("Середнє значення:", average_sales)
+
